a7be24d3e06388b9125ef86f9b3d674e4cf0afa5
Guilherme200 <guimatoso15@gmail.com>
2025-04-08 18:05:23 -0300

    Adicionar github actions

--- laravel/php8-2/app/build.sh
+++ laravel/php8-2/app/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Criar imagem
-docker build -t matoso30/laravel-app:8.2 .
-
-# Criar tag
-docker tag matoso30/laravel-app:8.2 matoso30/laravel-app:8.2
-
-# Subir imagem para docker hub
-docker push matoso30/laravel-app:8.2
+docker build -t matoso30/laravel-app:8.2-arm .
+#
+## Criar tag
+docker tag matoso30/laravel-app:8.2-arm matoso30/laravel-app:8.2-arm
+#
+## Subir imagem para docker hub
+docker push matoso30/laravel-app:8.2-arm
